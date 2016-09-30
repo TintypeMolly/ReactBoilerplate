9f7c44ad0ad867fec08bfc924a1c0be6950c5ef5
Oh Jinkyun <tintypemolly@gmail.com>
2016-09-30 23:02:10 +0900

    start script using promise

--- scripts/start.js
+++ scripts/start.js
@@ -15,44 +15,66 @@ import {copyFavicons} from "./favicon";
 
 const clientCompiler = webpack(clientConfig);
 const serverCompiler = webpack(serverConfig);
-let isServerRunning = false;
+let initialBuild = true;
 
 clean();
 copyFavicons();
 
 taskStart("start");
-clientCompiler.run((err, stats) => {
-  handleWebpackError(err, stats);
+
+const handleError = (err, stats, reject) => {
+  if (err) {
+    reject(err);
+  }
+  const jsonStats = stats.toJson();
+  if (jsonStats.errors.length > 0) {
+    reject(jsonStats.errors);
+  }
+};
+
+const clientBuild = new Promise((resolve, reject) => {
+  clientCompiler.run((err, stats) => {
+    handleError(err, stats, reject);
+    resolve();
+  });
+});
+
+const serverBuild = new Promise((resolve, reject) => {
   serverCompiler.watch({}, (err, stats) => {
-    handleWebpackError(err, stats);
-    if (!isServerRunning) {
-      taskEnd("start");
-      isServerRunning = true;
-
-      // run browsersync
-      const bsServer = browserSync.create();
-      bsServer.init({
-        port: DEV_SERVER_PORT,
-        middleware: [
-          webpackDevMiddleware(clientCompiler, {
-            publicPath: clientConfig.output.publicPath,
-            stats: clientConfig.stats,
-          }),
-          webpackHotMiddleware(clientCompiler),
-        ],
-        proxy: `http://localhost:${PORT}`,
-      });
-
-      // run server
-      const monitor = nodemon({
-        script: path.resolve(__dirname, "../build/server.js"),
-      });
-      process.once("SIGINT", () => {
-        monitor.once("exit", () => {
-          process.exit();
-        });
-      });
+    handleError(err, stats, reject);
+    if (initialBuild) {
+      initialBuild = false;
+      resolve();
+    } else {
+      serverCompiler.run(handleWebpackError);
     }
-    serverCompiler.run(handleWebpackError);
+  });
+});
+
+Promise.all([clientBuild, serverBuild]).then(() => {
+  taskEnd("start");
+
+  // run browsersync
+  const bsServer = browserSync.create();
+  bsServer.init({
+    port: DEV_SERVER_PORT,
+    middleware: [
+      webpackDevMiddleware(clientCompiler, {
+        publicPath: clientConfig.output.publicPath,
+        stats: clientConfig.stats,
+      }),
+      webpackHotMiddleware(clientCompiler),
+    ],
+    proxy: `http://localhost:${PORT}`,
+  });
+
+  // run server
+  const monitor = nodemon({
+    script: path.resolve(__dirname, "../build/server.js"),
+  });
+  process.once("SIGINT", () => {
+    monitor.once("exit", () => {
+      process.exit();
+    });
   });
 });
