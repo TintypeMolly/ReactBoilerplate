1c5e3dfa2561ecf5bc9a2d3591cb2c0973c2b86c
Oh Jinkyun <tintypemolly@gmail.com>
2016-10-01 05:16:19 +0900

    IndexPage can be functional component

--- src/components/structures/App.js
+++ src/components/structures/App.js
@@ -2,16 +2,12 @@ import React, {Component} from "react";
 import normalize from "normalize.css/normalize.css";
 import withStyles from "isomorphic-style-loader/lib/withStyles";
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Hello, World!</h1>
-        <p>this is a boiler plate</p>
-        {this.props.children}
-      </div>
-    );
-  }
-}
+const App = props => (
+  <div>
+    <h1>Hello, World!</h1>
+    <p>this is a boiler plate</p>
+    {props.children}
+  </div>
+);
 
 export default withStyles(normalize)(App);
