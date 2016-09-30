994c069296709143ada297d1b5771dffd8978415
Oh Jinkyun <tintypemolly@gmail.com>
2016-09-30 16:00:11 +0900

    put favicon into html.js

--- src/components/structures/Html/index.js
+++ src/components/structures/Html/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 
+import favicon from "./favicon";
+
 const Html = props => (
   <html>
   <head>
     <title>{props.title}</title>
     <meta charSet="utf-8"/>
+    {favicon}
     {props.style && <style id="css" dangerouslySetInnerHTML={{__html: props.style}}/>}
   </head>
   <body>
