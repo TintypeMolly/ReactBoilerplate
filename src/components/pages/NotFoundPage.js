a54ddc310855173d893a06c8fc46b849b8d5bef5
Oh Jinkyun <tintypemolly@gmail.com>
2016-10-07 21:27:32 +0900

    better status code dealing

--- src/components/pages/NotFoundPage.js
+++ src/components/pages/NotFoundPage.js
@@ -1,10 +1,22 @@
-import React from "react";
+import React, {Component, PropTypes} from "react";
 
-const NotFoundPage = () => (
-  <div>
-    <h1>Page Not Found</h1>
-    <p>The page you are looking for does not exist :(</p>
-  </div>
-);
+class NotFoundPage extends Component {
+  static contextTypes = {
+    setStatus: PropTypes.func,
+  };
+
+  componentWillMount() {
+    this.context.setStatus(400);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Page Not Found</h1>
+        <p>The page you are looking for does not exist :(</p>
+      </div>
+    );
+  }
+}
 
 export default NotFoundPage;
