2588f7014c2b19648226de873c219f4be1765a54
Oh Jinkyun <tintypemolly@gmail.com>
2016-10-22 08:03:46 +0900

    test to work on Windows 10

--- scripts/util.js
+++ scripts/util.js
@@ -1,3 +1,5 @@
+import spawn from "cross-spawn";
+
 /* eslint-disable no-console */
 const taskStart = name => console.log(`----- task "${name}" started -----`);
 const taskEnd = name => console.log(`-----  task "${name}" ended  -----`);
@@ -9,8 +11,29 @@ const catchPromiseReject = promise => promise.catch(error => {
   process.exit(1);
 });
 
+const spawnPromise = (command, args, options) => new Promise((resolve, reject) => {
+  const childProcess = spawn(command, args, options);
+  childProcess.stdout.on("data", data => {
+    process.stdout.write(data);
+  });
+  childProcess.stderr.on("data", data => {
+    process.stderr.write(data);
+  });
+  childProcess.on("close", code => {
+    if (code === 0) {
+      resolve(code);
+    } else {
+      reject(`Child process "${command}" exited with code ${code}`);
+    }
+  });
+  childProcess.on("error", err => {
+    reject(err);
+  });
+});
+
 export {
   taskStart,
   taskEnd,
   catchPromiseReject,
+  spawnPromise as spawn,
 };
