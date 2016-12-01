9765ef21c1b5b68ca83b4b67a307d3abc8990df8
Oh Jinkyun <tintypemolly@gmail.com>
2016-12-01 11:18:10 +0900

    lint

--- test/test.js
+++ test/test.js
@@ -8,6 +8,7 @@ describe("react-boilerplate", () => {
   it("main page", done => {
     request(app).get("/").expect(200, done);
   });
+  // eslint-disable-next-line no-undef
   it("display 404", done => {
     request(app).get("/no_such_path").expect(404, done);
   });
